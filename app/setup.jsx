ac345ae662d424642507b2b162cce05f59827f1b
Mah Chee Teng <mahcheeteng@r-219-99-25-172.comp.nus.edu.sg>
2023-07-21 19:12:35 +0800

    Update Calibrate Preference Page UI

--- app/setup.jsx
+++ app/setup.jsx
@@ -44,201 +44,201 @@ export default function Setup() {
     }
 
     return (
-    <SafeAreaView>
-        <View style = {{backgroundColor:'white'}}>
-            <IconButton style = {{backgroundColor: 'white'}} icon = 'arrow-left' onPress={() => router.back()}/>
+    <SafeAreaView style= {{backgroundColor: "#FFCECE"}}>
+        <View style = {{backgroundColor:'#FFCECE'}}>
+            <IconButton style = {{backgroundColor: '#FFCECE'}} icon = 'arrow-left' onPress={() => router.back()}/>
         </View>
         <ScrollView>
-            <View>
-                <Text>If you have already done the set up before, resubmitting will reset all past data</Text>
-                <Text>Set value of cuisine to 0 if you do not want to be recommend that cuisine at all</Text>
-                <Text>Rating:</Text>
-                <Text>1 - Recommend me this seldomly</Text>
-                <Text>10- Recommend me this frequently</Text>
+            <View style={styles.textBlock}>
+                <Text style= {styles.bigTexts}>Resubmitting will reset all past data {"\n"}</Text>
+                <Text style= {styles.texts}>0 - Do not want to be recommended this cuisine</Text>
+                <Text style= {styles.texts}>1 - Recommend me this seldomly</Text>
+                <Text style= {styles.texts}>10- Recommend me this frequently</Text>
             </View>
 
-            <View>
-                <Text>Chinese : {value1}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value1}
-                        onValueChange={(a) => setValue1(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+            <View style= {styles.content}>
+                <View>
+                    <Text>Chinese : {value1}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value1}
+                            onValueChange={(a) => setValue1(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Malaysian : {value2}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value2}
-                        onValueChange={(a) => setValue2(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Malaysian : {value2}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value2}
+                            onValueChange={(a) => setValue2(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Indian : {value3}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value3}
-                        onValueChange={(a) => setValue3(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Indian : {value3}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value3}
+                            onValueChange={(a) => setValue3(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Japanese : {value4}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value4}
-                        onValueChange={(a) => setValue4(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Japanese : {value4}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value4}
+                            onValueChange={(a) => setValue4(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Korean : {value5}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value5}
-                        onValueChange={(a) => setValue5(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Korean : {value5}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value5}
+                            onValueChange={(a) => setValue5(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Vietnamese : {value6}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value6}
-                        onValueChange={(a) => setValue6(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Vietnamese : {value6}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value6}
+                            onValueChange={(a) => setValue6(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Thai : {value7}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value7}
-                        onValueChange={(a) => setValue7(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Thai : {value7}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value7}
+                            onValueChange={(a) => setValue7(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Indonesian : {value8}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value8}
-                        onValueChange={(a) => setValue8(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Indonesian : {value8}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value8}
+                            onValueChange={(a) => setValue8(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View>
-                <Text>Vegetarian : {value9}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value9}
-                        onValueChange={(a) => setValue9(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+                <View>
+                    <Text>Vegetarian : {value9}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value9}
+                            onValueChange={(a) => setValue9(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
                 </View>
-            </View>
+                
+                <View>
+                    <Text>Western : {value10}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value10}
+                            onValueChange={(a) => setValue10(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <Text>Italian : {value11}</Text>
+                    <View style = {styles.slider}>
+                        <Text style = {{margin: 10}}>0</Text>
+                        <Slider
+                            style = {{flex: 1}}
+                            minimumValue={0}
+                            maximumValue={10}
+                            value={value11}
+                            onValueChange={(a) => setValue11(Math.trunc(a))}
+                            tapToSeek={false}
+                        />
+                        <Text style = {{margin: 10}}>10</Text>
+                    </View>
+                </View>
             
-            <View>
-                <Text>Western : {value10}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value10}
-                        onValueChange={(a) => setValue10(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
+
+                <View style = {{marginBottom: 50}}>
+                    <Button onPress={handleSubmit}><Text style= {{color: "#5A1B1B"}}>Submit</Text></Button>
+                    {errMsg !== "" && <Text>{errMsg}</Text>}
                 </View>
             </View>
-
-            <View>
-                <Text>Italian : {value11}</Text>
-                <View style = {styles.slider}>
-                    <Text style = {{margin: 10}}>0</Text>
-                    <Slider
-                        style = {{flex: 1}}
-                        minimumValue={0}
-                        maximumValue={10}
-                        value={value11}
-                        onValueChange={(a) => setValue11(Math.trunc(a))}
-                        tapToSeek={false}
-                    />
-                    <Text style = {{margin: 10}}>10</Text>
-                </View>
-            </View>
-        
-
-            <View style = {{marginBottom: 50}}>
-                <Button onPress={handleSubmit}>Submit</Button>
-                {errMsg !== "" && <Text>{errMsg}</Text>}
-            </View>
-
         </ScrollView>
     </SafeAreaView>
     )
@@ -248,5 +248,38 @@ const styles = StyleSheet.create({
     slider: {
         flexDirection:'row',
         alignItems:'center',
+    },
+
+    textBlock: {
+        backgroundColor: "#FB9999",
+        borderRadius: 30,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        width: 397,
+        height: "17%",
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+
+    bigTexts: {
+        color: "white",
+        alignSelf: "center",
+        fontWeight: 600,
+        fontSize: 20
+    },
+
+    texts: {
+        color: "white",
+        alignSelf: "center",
+        fontWeight: 600,
+    },
+
+    content: {
+        paddingTop: 10,
+        flex: 1,
+        paddingHorizontal: 10,
+        backgroundColor: "#FFCECE",
     }
 });

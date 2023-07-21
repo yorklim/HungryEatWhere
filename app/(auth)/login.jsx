bc3c05ddb135eea175f66051a3d692d851eaa14c
Mah Chee Teng <mahcheeteng@r-219-99-25-172.comp.nus.edu.sg>
2023-07-21 15:04:08 +0800

    update Login Page UI

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { useState } from "react";
 import { Text, TextInput, Button, ActivityIndicator } from "react-native-paper";
 import { Link } from "expo-router";
@@ -28,31 +28,109 @@ export default function LoginPage() {
         }
     }
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text>Email</Text>
-            <TextInput
-                placeholder="Username"
-                autoCapitalize='none'
-                textContentType='emailAddress'
-                value={email}
-                onChangeText={setEmail} />
-            <Text>Password</Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>Indecisive Foodie</Text>
+            </View>
+            <View style={styles.subContainer}>
+                <View style= {{margin: 5}}>
+                    <Text style={styles.inputText}>Email</Text>
+                </View>
+                <TextInput
+                    placeholder="Email"
+                    autoCapitalize='none'
+                    textContentType='emailAddress'
+                    value={email}
+                    onChangeText={setEmail}
+                    style={styles.inputContainer} />
+            </View>
+            <View style={styles.subContainer}>
+            <View style= {{margin: 5}}>
+                <Text style={styles.inputText}>Password</Text>
+            </View>
             <TextInput
                 placeholder="Password"
                 secureTextEntry={true}
                 autoCapitalize='none'
                 textContentType='password'
                 value={password}
-                onChangeText={setPassword} />
-            <Button 
-                onPress={handleSubmit}
-                testID="loginButton"
-            >Login</Button>
-            {errMsg !== "" && <Text>{errMsg}</Text>}
-            {loading && <ActivityIndicator />}
-            <Link href="/register">
-                <Button>Go to register</Button>
-            </Link>
+                onChangeText={setPassword}
+                style={styles.inputContainer} />
+            </View>
+            <View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity 
+                        onPress={handleSubmit}
+                        testID="loginButton" >
+                        <Text style= {styles.textButton}>Login</Text>
+                    </TouchableOpacity>
+                    <Link href="/register">
+                        <TouchableOpacity>
+                        <Text style= {styles.textButton}>Go to register</Text>
+                        </TouchableOpacity>
+                    </Link>
+                    
+                </View>
+                <View style={styles.errorContainer}>
+                    {errMsg !== "" && <Text style={styles.errorMsg}>{errMsg}</Text>}
+                    {loading && <ActivityIndicator />}
+                </View>
+            </View>
         </View>
     )
 }
+
+// need to add an overall <view> to add padding to everything
+// so its not all stuck to the side
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#FB9999"
+    },
+    subContainer: {
+        padding: 10,
+    },
+    inputText: {
+        color: "white",
+        marginHorizontal: 15,
+        fontSize: 11
+    },
+    inputContainer: {
+        borderRadius: 30,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        borderWidth: 3,
+        borderColor: "#FFDEAC",
+        backgroundColor: "#FFEDD2",
+    },
+    textButton: {
+        fontWeight: "bold",
+        color: "white"
+    },
+    buttonContainer: {
+        flexDirection: 'row', 
+        justifyContent: "space-around",
+        margin: 5,
+        marginTop: 20,
+    },
+    errorMsg: {
+        fontWeight: "bold",
+        color: "red",
+        alignSelf: "center"
+    },
+    errorContainer: {
+        width: "100%",
+        padding: 20,
+    },
+    headerContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20
+    },
+    headerText: {
+        fontWeight: "bold",
+        fontSize: 40,
+        color: 'white',
+    }
+})
